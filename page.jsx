d7200389aeb68b9622a6cccd4d3e3814faff535a
Naufal786-hub <kopitraderscommunity@gmail.com>
2025-07-11 14:17:26 +0700

    Delete page.jsx

--- page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ backgroundColor: 'black', color: 'white', padding: '2rem' }}>
-      <h1 style={{ color: 'green' }}>Kopitraders Community</h1>
-      <p>Kode Akses: NgopiLur2025</p>
-    </main>
-  );
-}
